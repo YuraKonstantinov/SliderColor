2752b531c8671755e5705cbf30e0d4e236d70954
YuraKonstantinov <georgekonstantinov@yandex.ru>
2021-08-22 23:25:44 +0400

    View Controller

--- SliderColor/ViewController.swift
+++ SliderColor/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var viewColor: UIView!
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    
+    @IBOutlet var sliderRedView: UISlider!
+    @IBOutlet var sliderGreenView: UISlider!
+    @IBOutlet var sliderBlueView: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewColor.layer.cornerRadius = 10
+        
     }
-
+    
+    @IBAction func sliderColor() {
+        labelRed.text = String(sliderRedView.value)
+        labelGreen.text = String(sliderGreenView.value)
+        labelBlue.text = String(sliderBlueView.value)
+        
+        viewColor.backgroundColor = UIColor(red: CGFloat(sliderRedView.value), green: CGFloat(sliderGreenView.value), blue: CGFloat(sliderBlueView.value), alpha: 1)
+    }
+    
+    
+    
+    
 
 }
 
